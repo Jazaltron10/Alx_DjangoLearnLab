4ed5a47048f4432945281f31548874ee74cb9e58
Jazaltron <jazaltron10@gmail.com>
2024-08-18 21:51:35 +0100

    Task 1 - try-005

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -8,17 +8,13 @@ def list_books(request):
     books = Book.objects.all()
 
     # Render the list_books.html template, passing the list of books to the template
-    return render(request, 'list_books.html', {'books': books})
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 
 
 # Define the class-based view to display library details
 class LibraryDetailView(DetailView):
     model = Library  # Specify the model to use for this view
-    template_name = 'library_detail.html'  # Specify the template to render
+    template_name = 'relationship_app/library_detail.html'  # Specify the template to render
 
-    # Override the context data to include related books
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['books'] = self.object.books.all()  # Get all books related to the library
-        return context
+    context_object_name = 'library'
