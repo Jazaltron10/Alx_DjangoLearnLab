01c83ceae715ce34a0a52b39628578d728341bcc
Jazaltron <jazaltron10@gmail.com>
2024-08-27 13:29:05 +0100

    API_Task_1_[Endpoint] - try-003

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from .views import BookList
 
 urlpatterns = [
-    path("", BookList.as_view(), name="home"),
-    path("api/books", BookList.as_view(), name="book_list_create"),
+    # path("", BookList.as_view(), name="home"),
+    path('books/', BookList.as_view(), name='book-list'),
 ]
+
+
+
