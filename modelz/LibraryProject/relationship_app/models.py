93bc9f6e68a45ba0466a8571dc8e968b9a107280
Jazaltron <jazaltron10@gmail.com>
2024-08-31 17:17:41 +0100

    Routine Update

--- modelz/LibraryProject/relationship_app/models.py
+++ modelz/LibraryProject/relationship_app/models.py
@@ -60,9 +60,9 @@ class Book(models.Model):
     
     class Meta:
         permissions = [
-         ("can_add_book","Can add book"),   
-         ("can_change_book","Can change book"),   
-         ("can_delete_book","Can delete book"),   
+        ("can_add_book","Can add book"),   
+        ("can_change_book","Can change book"),   
+        ("can_delete_book","Can delete book"),   
         ]
     
     # String representation of the Book object to return the book's title and author for easy identification.
