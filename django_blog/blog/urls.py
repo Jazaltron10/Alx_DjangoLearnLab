73c884c6d35bfb364f0d3304e9d718eb875bd17d
Jazaltron <jazaltron10@gmail.com>
2024-09-15 21:30:32 +0100

    DJANGO_BLOG_Task_2_[POST_MANAGEMENT] - try-004

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('', PostListView.as_view(), name='post-list'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
-    path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path("register/", register, name="register"),
     path("login/", auth_views.LoginView.as_view(template_name="blog/login.html"), name="login"),
