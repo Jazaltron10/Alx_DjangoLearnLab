38e0a1f3c794d79a62061049b68edfc45501d60c
Jazaltron <jazaltron10@gmail.com>
2024-09-15 17:33:31 +0100

    DJANGO_BLOG_Task_1_[AUTHENTICATION] - try-002

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -4,8 +4,8 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("", home, name="home"),
-    path("register", register, name="register"),
-    path("login", auth_views.LoginView.as_view(template_name="blog/login.html"), name="login"),
+    path("register/", register, name="register"),
+    path("login/", auth_views.LoginView.as_view(template_name="blog/login.html"), name="login"),
     path("logout", auth_views.LogoutView.as_view(), name="logout"),
     path('profile/', profile, name='profile'),
 ] 
